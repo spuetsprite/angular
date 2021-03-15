67e240ed241c22624ba06ebfbb984a18631c6150
spuetsprite <jittakan.tum@student.mahidol.edu>
2021-03-15 18:17:39 +0700

    Initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+
+
+import { Component, Input } from '@angular/core' ;
+
+@Component({
+    selector: 'app-home' ,
+    templateUrl: './home.component.html'
+})
+export class HomeComponent {
+    @Input()
+    title: string;
+    constructor () {}
+}
